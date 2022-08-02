13fc2de329f7b06f311290077f9eb117850d68f2
aCrockofSchmidt <61259152+aCrockofSchmidt@users.noreply.github.com>
2022-08-02 10:28:53 -0600

    Update main.py
    Changed code to remove twilio security breach

--- Day 35 - Weather Alert SMS/main.py
+++ Day 35 - Weather Alert SMS/main.py
@@ -6,11 +6,10 @@ import os
 
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-#api_key = "626a0d3b4dc12dd9ad2b9f76a7588a7c"
 api_key = os.environ.get("OWM_API_KEY")
 
 account_sid = "AC7bfad92a7d8c6c80bbc36d26aab22574"
-auth_token = "5820050e9f760ce18f129ce634e68c66"
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 
 weather_params = {
     "lat": 51.048615,
@@ -27,17 +26,17 @@ weather_data = response.json()
 
 will_rain = False
 
-for days in range(11):
+for days in range(47):
     if weather_data["hourly"][days]["weather"][0]["id"] < 700:
         will_rain = True
 
+
 if will_rain:
     client = Client(account_sid, auth_token)
-    message = client.messages \
-        .create(
+    message = client.messages.create(
         body="It's going to rain today. Remember to bring an umbrella.",
         from_='+18482175388',
         to='+15877774433'
     )
-print(message.status)
+    print(message.status)
 
